b9631aada8e818abafa084b3642b1e2d3ac608cb
Kadar Hall <kadar.hall@campingworld.com>
2023-11-25 17:44:03 -0500

    package updates

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,10 +1,10 @@
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+// import { mockDataTeam } from "../../data/mockData";
+// import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+// import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+// import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
 import { getCurrentMovies } from "../../data/moviesData";
 import { Image } from "@mui/icons-material";
